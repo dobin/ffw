8f799de96dc31701aab93ae9d1e228d2e792de00
Dobin Rutishauser <dobin@broken.ch>
2017-07-28 05:38:59 -0400

    more reliable crash binning, with asan output

--- bin_crashes.py
+++ bin_crashes.py
@@ -22,6 +22,7 @@ from multiprocessing import Process, Queue
 
 import framework
 
+
 # This is a Queue that behaves like stdout
 class StdoutQueue():
     def __init__(self,*args,**kwargs):
@@ -38,19 +39,26 @@ class StdoutQueue():
 # it will start the server as his own child
 # it will communicate crash report of the child (server) to the parent
 def startServer(config, queue_sync, queue_out):
-    sys.stdout = StdoutQueue(queue_out)
+    # Sadly this does not apply to child processes started via
+    # createChild(), so we can only capture output of this python process
+    stdoutQueue = StdoutQueue(queue_out)
+    #sys.stdout = stdoutQueue
+    #sys.stderr = stdoutQueue
+
+    queue_out.put("Dummy")
+    # do not remove print, parent excepts something
     print "Start Server"
+    #sys.stderr.write("Stderr")
 
     # create child via ptrace debugger
-    # API: createChild(arguments, no_stdout, env=None)
-    # TODO: Set env
+    # API: createChild(arguments[], no_stdout, env=None)
     pid = createChild(
         [
             config["target_bin"],
-            str(config["target_port"])
+            str(config["target_port"]),
         ],
         False,
-        None
+        None,
     )
 
     # Attach to the process with ptrace and let it run
@@ -59,10 +67,11 @@ def startServer(config, queue_sync, queue_out):
     proc.cont()
 
     # notify parent about the pid
-    queue_sync.put(pid)
+    queue_sync.put( ("pid", pid) )
 
     event = None
     while True:
+        print "T: Wait for process"
         event = dbg.waitProcessEvent()
 
         # If this is a process exit we need to check if it was abnormal
@@ -84,12 +93,43 @@ def startServer(config, queue_sync, queue_out):
     # send crash details
     # Note: If the server does not crash, we kill it in the parent.
     #       This will still generate a (unecessary) "crash" message and will be sent here
-    # TODO fixme
-    if event is not None: 
+    # TODO fixme maybe
+    if event is not None and event.signum != 15: 
+        print "T: Crash"
         data = getCrashDetails(event)
-        queue_sync.put(data)
+        queue_sync.put( ("data", data) )
+    else: 
+        data = ()
+        print "T: NO crash"
+        queue_sync.put( ("data", data) )
 
     dbg.quit()
+
+
+
+def getAsanOutput(config, pid): 
+    # as we cannot get stdout/stderr of child process, we store asan
+    # output in the temp folder in the format: asan.<pid>
+    fileName = config["temp_dir"] + "/asan." + str(pid)
+    print "Get asan output"
+    print "  filename: " + str(fileName)
+
+    time.sleep(0.5) # omg wait for the file to appear
+
+    if not os.path.isfile(fileName):
+        print "  !!!!!!!!! NO ASAN OUTPUT !!!"
+        return "  !!!!!!!!! NO ASAN OUTPUT !!!"
+
+    # it may not exist, which aint bad (e.g. no asan support)
+    file = open(fileName, "r")
+    data = file.read()
+    print "  Got ASAN OUTPUT: " + str(len(data))
+    file.close()
+
+    # remove the file, as we dont need it anymore
+    os.remove(fileName)
+
+    return data
 
 
 def minimize(config):
@@ -105,9 +145,11 @@ def minimize(config):
     outcomesDir = os.path.abspath(config["outcome_dir"])
     outcomes = glob.glob(os.path.join(outcomesDir, '*.raw'))
 
+    framework._setupEnvironment(config)
     print "Processing %d outcomes" % len(outcomes)
 
     for outcome in outcomes:
+        print "\n\n Now: " + str(n) + ": " + outcome
         config["target_port"] = config["baseport"] + n 
         n += 1
 
@@ -116,19 +158,26 @@ def minimize(config):
         p.start()
 
         # wait for ok (pid) from child that the server has started
-        serverPid = queue_sync.get()
-        time.sleep(0.2) # wait a bit till server is ready
+        data = queue_sync.get()
+        serverPid = data[1]
+        print "M: Server pid: " + str(serverPid)
+        time.sleep(1) # wait a bit till server is ready
         while not framework.testServerConnection(config):
             print "Server not ready, waiting and retrying"
-            time.sleep(0.2) # wait a bit till server is ready
+            time.sleep(0.1) # wait a bit till server is ready
         
+        print "M: Send"
         framework.sendDataToServer(config, outcome)
 
         # get crash result data
         # or empty if server did not crash
         try:
+            print "M: Wait for crash data"
             crashData = queue_sync.get(True, 1)
+            print "M: Crash!"
+            crashData = crashData[1]
             crashOutput = queue_out.get()
+            asanOutput = getAsanOutput(config, serverPid)
             details = crashData[3]
             signature = ( crashData[0], crashData[1], crashData[2] )
             details = {
@@ -137,16 +186,29 @@ def minimize(config):
                 "signature": crashData[2],
                 "gdbdetails": crashData[3],
                 "output": crashOutput,
+                "asan": asanOutput,
                 "file": outcome,
             }
             crashes[signature] = details
             storeValidCrash(config, signature, details)
-        except:
+        except Exception as error:
+            print "M: Waited long enough, NO crash. "
+            print "Exception: " + str(error)
             # timeout waiting for the data, which means the server did not crash
             # kill it, and receive the unecessary data
-            os.kill(serverPid, signal.SIGTERM)
-            notneeded1 = queue_sync.get()
-            crashOutput = queue_out.get()
+            # TODO: If os.kill throws an exception, it could not kill it, therefore
+            #       the start of the server failed. Retry
+            try: 
+                os.kill(serverPid, signal.SIGTERM)
+            except:
+                print "  M: !!!!!!!!!!! Exception: Could not kill :-("
+
+            try: 
+                notneeded1 = queue_sync.get(True, 1)
+                crashOutput = queue_out.get(True, 1)
+            except:
+                print "  M: !!!!!!!!!!! Exception: No data to get for non-crash :-("
+
 
         # wait for child to exit
         p.join()
@@ -160,13 +222,15 @@ def minimize(config):
 
 
 def storeValidCrash(config, crashSig, crashDetail):
-    with open(os.path.join(config["outcome_dir_bin"], crashDetail["file"] + ".crashdata.txt"), "w") as f:
+    with open(os.path.join(config["outcome_dir"], crashDetail["file"] + ".crashdata.txt"), "w") as f:
         f.write("Offset: %s\n" % crashDetail["faultOffset"])
         f.write("Module: %s\n" % crashDetail["module"])
         f.write("Signature: %s\n" % crashDetail["signature"])
         f.write("Details: %s\n" % crashDetail["gdbdetails"])
         f.write("Time: %s\n" % time.strftime("%c"))
         f.write("Output:\n %s\n" % crashDetail["output"])
+        f.write("\n")
+        f.write("ASAN Output:\n %s\n" % crashDetail["asan"])
         f.close()
 
 
