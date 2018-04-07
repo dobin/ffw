94a4b83efaacb6f3b0f0abf34517a826695c8992
dobin <dobin@broken.ch>
2018-04-07 15:00:01 +0200

    add ffw.py

---
+++ ffw.py
@@ -0,0 +1,20 @@
+import framework
+
+import sys
+import ast
+
+"""
+The first way to use FFW is to create a subdirectory in ffw/, and then
+copy the fuzzing.py from template/
+
+This is kinda suboptimal, so this ffw.py is able to read a config file
+similar to fuzzing.py instead.
+"""
+
+
+def main():
+    framework.realMain(None)
+
+
+if __name__ == '__main__':
+    sys.exit(main())
