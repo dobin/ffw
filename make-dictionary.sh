6ae33b9c4f885650ac4d3f70b846d922ce9c8dec
dobin <dobin@broken.ch>
2018-05-19 21:11:02 +0200

    better dictionary maker

--- make-dictionary.sh
+++ make-dictionary.sh
@@ -1,4 +1,29 @@
 #!/bin/bash
 
-objdump -d "${1}" | grep -Eo '\$0x[0-9a-f]+' | cut -c 2- | sort -u | while read const; do echo $const | python -c 'import sys, struct; sys.stdout.write("".join(struct.pack("<I" if len(l) <= 11 else "<Q", int(l,0)) for l in sys.stdin.readlines()))' > testcases/$const; done
-i=0; strings "${1}"| while read line; do echo "$line" >> dictionary.txt ; i=$[ $i + 1 ] ; done
+# via https://stackoverflow.com/questions/3925075/how-to-extract-only-the-raw-contents-of-an-elf-section
+
+IN_F=$1
+OUT_F=./tmp1.bin
+SECTION=.rodata
+
+objdump -h $IN_F |
+  grep $SECTION |
+    awk '{print "dd if='$IN_F' of='$OUT_F' bs=1 count=$[0x" $3 "] skip=$[0x" $6 "]"}' |
+      bash
+
+
+SECTION=.data
+OUT_F=./tmp2.bin
+objdump -h $IN_F |
+  grep $SECTION |
+    awk '{print "dd if='$IN_F' of='$OUT_F' bs=1 count=$[0x" $3 "] skip=$[0x" $6 "]"}' |
+      bash
+
+strings ./tmp1.bin > dict
+strings ./tmp2.bin >> dict
+
+cat dict | egrep -v '\%|\*|\-\-|\.c|\(\)' | sed '/^.\{16\}./d' > dict2
+
+sort -u dict2 > dictionary.txt
+
+rm tmp1.bin tmp2.bin dict dict2
